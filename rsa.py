3736df3e59031de1f5f77d0cd33d717500374f98
SamDad <55953103+SamDad@users.noreply.github.com>
2019-10-23 16:11:14 +0300

    Create rsa.py
        rsa.decrypt
        rsa.encrypt
        rsa.extended_gcd
        rsa.generate_keypair
    3 items passed all tests:
       2 tests in rsa.gcd
       3 tests in rsa.is_prime
       1 tests in rsa.multiplicative_inverse
    6 tests in 8 items.
    6 passed and 0 failed.
    Test passed.

---
+++ rsa.py
@@ -0,0 +1,104 @@
+from math import sqrt
+import random
+
+
+def is_prime(n: int) -> bool:
+    """
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
+    if n == 1:
+        return False
+    for i in range(2, int(sqrt(n)) + 1):
+        if not n % i:
+            return False
+    return True
+
+
+def gcd(a: int, b: int) -> int:
+    """
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    while a and b:
+        if a > b:
+            a %= b
+        else:
+            b %= a
+    if a > 0:
+        return  a
+    else:
+        return b
+
+
+def extended_gcd(a: int, b: int):
+    x0, x1, y0, y1 = 0, 1, 1, 0
+    while a != 0:
+        q, b, a = b // a, a, b % a
+        y0, y1 = y1, y0 - q * y1
+        x0, x1 = x1, x0 - q * x1
+    return b, x0, y0
+
+
+def multiplicative_inverse(e: int, phi: int) -> int:
+    """
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+    g, x, y = extended_gcd(e, phi)
+    return x % phi
+
+
+def generate_keypair(p: int, q: int):
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+
+    n = p * q
+
+    phi = (p-1) * (q-1)
+    e = random.randrange(1, phi)
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+    d = multiplicative_inverse(e, phi)
+    return ((e, n), (d, n))
+
+
+def encrypt(pk, plaintext):
+    key, n = pk
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    return cipher
+
+
+def decrypt(pk, ciphertext):
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return ''.join(plain)
+
+
+if __name__ == '__main__':
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print(''.join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
